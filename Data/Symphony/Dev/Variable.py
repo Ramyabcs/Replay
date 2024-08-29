2964fc3d8b66db2ff1ab8d00ae51ed0b2c491702
khadhijah <mailtokhadhijah@gmail.com>
2024-08-29 11:19:57 +0530

    first push

---
+++ Data/Symphony/Dev/Variable.py
@@ -0,0 +1,13 @@
+EXE_PAD = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
+Connection_Name = "RBT"
+SAP_CLIENT = "000"
+SAP_USER = "DDIC"
+#SAP_PASSWORD = "Sym@rocks2023"
+addOn = ["BNWVS",    "ST-A/PI"]     
+Patch = ["SAPK-70001INBNWVS",    "SAPKITABC5"]
+patch_comp = ["ST-PI", "ST-A/PI"]
+patch_vers = ["SAPK-74022INSTPI", "SAPKITABC5"]
+
+
+
+
